4722493dfde0bd3f1d51beeb1cec85bb0677cfb4
Michael Doan <babean@users.noreply.github.com>
2016-06-28 10:37:11 -0500

    Make it a library

--- mpico.h
+++ mpico.h
@@ -1,7 +1,25 @@
-#define SS_PIN D5
-#define _TCBUSY_PIN D6
+#ifndef _TCON_MPICO_
+#define _TCON_MPICO_
 
-#define LEMARKER 0x00
+#if defined (PARTICLE)
+#include "application.h"
+#else
+#include "Arduino.h"
+#endif
+
+#if PLATFORM_ID == 0 // Core
+#define MYSPI SPI
+#elif PLATFORM_ID == 6 // Photon
+#define MYSPI SPI1
+#elif PLATFORM_ID == 8 // P1
+#define MYSPI SPI1
+#elif PLATFORM_ID == 10 // Electron
+#define MYSPI SPI1
+#elif PLATFORM_ID == 103 // BluzDK
+#define MYSPI SPI
+#else
+  #error "*** PLATFORM_ID not supported by this firmware. PLATFORM should be Core, Photon, P1 or Electron ***"
+#endif
 
 static char uploadImageData[3]=         {0x20,0x01,0x00};       //
 static char resetDataPointer[3]=        {0x20,0x0D,0x00};       //
@@ -12,79 +30,23 @@ static char getSystemInfo[4]=           {0x31,0x01,0x01,0x00};  //LE=0
 static char getSystemVersionCode[4]=    {0x31,0x02,0x01,0x10};  //LE=0x10
 static char readSensorData[4]=          {0xE5,0x01,0x00,0x02};  //LE=2
 
-//static char tc_p441_230_epd_hdr[16]=    {0x33,0x01,0x90,0x01,0x2C,0x01,0x01,0x00,
-//                                         0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00}; // type1
+class TCON_MPICO
+{
+public:
+  TCON_MPICO(uint8_t ss, int spiclkspeed, uint8_t tcbusy);
+  void begin(void);
+  void sendCommand(char *buffer, int size);
+  void getResponse(int le);
+  void uploadImage(char *buffer);
 
-void waitInit(){
-    delay(5);
-    if (digitalRead(_TCBUSY_PIN)) Serial.println("TCon initialized Completed");
-    else Serial.println("TCon initialized Failed");
-    delay(5);
-}
-void waitTCbusy(){
-    delayMicroseconds(20);
-    while (!digitalRead(_TCBUSY_PIN));
-}
+private:
+  uint8_t _ss;
+  uint8_t _tcbusy;
+  int _spiclkspeed;
 
-void sendCommand(char *buffer, int size){
-    waitTCbusy();
-    digitalWrite(SS_PIN,LOW);
-    for (int i=0;i<size;i++) {
-        SPI1.transfer(buffer[i]);
-    }
-    digitalWrite(SS_PIN,HIGH);
-}
-void getResponse(int le){
-    int data, data2;
-
-    waitTCbusy();
-    digitalWrite(SS_PIN,LOW);
-    if (le==0) {
-        data=SPI1.transfer(0x00);
-        while (data!=LEMARKER) {
-            printHex(data,0);
-            data=SPI1.transfer(0x00);
-        }
-    }
-    else if (le>0) {
-        for (int i=0;i<le;i++){
-            data=SPI1.transfer(0x00);
-            printHex(data,0);
-        }
-    }
-    Serial.print(" Status: ");
-    data=SPI1.transfer(0x00);
-    data2=SPI1.transfer(0x00);
-    digitalWrite(SS_PIN,HIGH);
-    if ((data == 0x90) && (data2 == 0x00)) Serial.println("Done");
-    else {
-        Serial.printf(" **%.2x%.2x\n", (unsigned)data, (unsigned)data2);
-    }
-    digitalWrite(SS_PIN,HIGH);
-}
-
-void uploadImage(char *buffer){
-    waitTCbusy();
-    digitalWrite(SS_PIN,LOW);
-    SPI1.transfer(0x20);
-    SPI1.transfer(0x01);
-    SPI1.transfer(0x00);
-    SPI1.transfer(0x10);
-    for (int i=0;i<16;i++) SPI1.transfer(buffer[i]);
-    digitalWrite(SS_PIN,HIGH);
-    Serial.print("Uploadheader: ");
-    getResponse(-1);
-    
-    waitTCbusy();
-    for (int i=0;i<60;i++){
-        waitTCbusy();
-        digitalWrite(SS_PIN,LOW);
-        SPI1.transfer(0x20);
-        SPI1.transfer(0x01);
-        SPI1.transfer(0x00);
-        SPI1.transfer(0xFA);
-        for (int j=0;j<250;j++) SPI1.transfer(buffer[16+(250*i)+j]);
-        digitalWrite(SS_PIN,HIGH);
-        getResponse(-1);
-    }
-}
+  void waitTCbusy(void);
+  void waitInit(void);
+  void printHex(int input, bool ln);
+  
+};
+#endif
